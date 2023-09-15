b05f89bb7ed8bf6ec4eea14a7553cc2ad2109529
unknown <gacombs@yahoo.com>
2023-09-14 23:14:19 -0500

    PWA that is downloadable and can load contacts from phone.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  ...withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  })
 }
+
 
 module.exports = nextConfig
